0481c17eeefc29ea41a7c3b469db4e55d5322c3f
manishaj <manisha.jain@tesco.com>
2021-04-16 15:17:08 +0530

    remove pub keys

--- backend.tf
+++ backend.tf
@@ -1,8 +1,7 @@
-#terraform {
-#  backend "s3" {
-#    bucket = "petclinic-terraform-state-9q5kn5fr"
-#    key    = "terraform.tfstate"
-#    region = "eu-west-1"
-
-#  }
-#}
+terraform {
+  backend "s3" {
+    bucket = "petclinic-terraform-state-o4wz4851"
+    key    = "terraform.tfstate"
+    region = "eu-west-1"
+  }
+}
